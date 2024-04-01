1a9ea8b1949c2cac945e8a022fc29c9138013c42
pooja97 <poojayadav2124@gmail.com>
2024-03-31 23:35:04 -0400

    Array

--- Arrays/Top_K_frequentElements.py
+++ Arrays/Top_K_frequentElements.py
@@ -4,24 +4,53 @@ Example 1:
 Input: nums = [1,1,1,2,2,3], k = 2
 Output: [1,2]
 
-Approach: Count the frequency of each element and create a HashMap out of it. Sort the hashmap in reverse order based on the hashmap values.
-then return the first k values from the hashmap. 
+Approach: Count the frequency of each element and create a HashMap out of it. Create a List[List] where in each index position will s
+erve as a dictionary count value and the values at that list index position will be the keys from the dictionary. 
+Traverse the list in reverse and append values in a result list until it's size is equal to k. 
 
+
+Run time: O(n)
 '''
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         d = dict()
+#         for i in nums:
+#             if i not in d:
+#                 d[i] = 1
+#             else:
+#                 d[i]+=1 
+
+#         d = dict(sorted(d.items(), key = lambda item:item[1], reverse = True))
+#         lst = list()
+
+#         for key in d.keys():
+#             lst.append(key)
+#             if len(lst) == k:
+#                 break
+#         return lst
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d = dict()
+        d = dict() 
+        l = [[] for i in range(len(nums)+1)]
+
         for i in nums:
+            # d[i] = 1+d.get(i,0) 
             if i not in d:
                 d[i] = 1
             else:
-                d[i]+=1 
+                d[i]+=1
+       
+        for key, val in d.items():
+            l[val].append(key)
+            
+        result = []
+        for i in range(len(l)-1,0,-1):
+            for j in l[i]:
+                result.append(j)
 
-        d = dict(sorted(d.items(), key = lambda item:item[1], reverse = True))
-        lst = list()
+            if len(result) == k:
+                return result
 
-        for key in d.keys():
-            lst.append(key)
-            if len(lst) == k:
-                break
-        return lst
+
+        
